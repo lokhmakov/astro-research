5c7a3be924221d30fd3ebadd76147cdebd0dc091
Pavel Lokhmakov <l.pavel.m@gmail.com>
2021-07-09 19:40:27 +0300

    Delete .history directory

--- .history/src/components/research2_20210708183611.jsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-
-export function Research2() {
-  const [value, setValue] = React.useState(0)
-
-  return <div>foo bar {value}</div>
-}
--- .history/src/components/research2_20210708183614.jsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-
-export function Research2() {
-  const [value] = React.useState(0)
-
-  return <div>foo bar {value}</div>
-}
